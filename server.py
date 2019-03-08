0b2c57486a36f0b3bf28d08a4a5ee756bf3e6ecb
Carmine White <c.white@ahkclean.com>
2019-03-07 22:53:05 -0800

    Uploading fruit dojo...finished assignment

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    fruit_count = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    print("Charging", request.form['first_name'], request.form['last_name'], "for", fruit_count, "fruits")
+    return render_template("checkout.html")
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
